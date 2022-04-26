8c3fc18e8ef9ae73b253ecae09fdd619b3a45d0a
akalankaih <akalankaih.19@uom.lk>
2022-04-27 02:24:16 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("Mongodb connection success.!");
+})
+
+app.listen(PORT, () => {
+  console.log("Server up and running on port:",PORT);
+})
